5d67b1d031e4cf37c2233b7c830908b6cded1de5
tokarvasyl1 <tokarvasyl1@gmail.com>
2022-08-20 14:15:16 +0300

    popup

---
+++ page/success.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="ua">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Успішна покупка</title>
+
+    <link rel="stylesheet" href="../assets/styles/scss/main.css">
+</head>
+<body>
+<div class="wrapper">
+    <nav class="nav">
+        <div class="_container">
+            <div class="nav__logo">
+                <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                     width="50.000000pt" height="50.000000pt" viewBox="0 0 440.000000 443.000000"
+                     preserveAspectRatio="xMidYMid meet">
+
+                    <g transform="translate(0.000000,443.000000) scale(0.100000,-0.100000)"
+                       fill="black" stroke="none">
+                        <path d="M1965 4403 c-284 -33 -533 -106 -767 -224 -426 -214 -783 -583 -985
+-1019 -274 -592 -268 -1269 17 -1855 361 -743 1100 -1212 1920 -1218 211 -2
+324 10 518 54 l93 21 -37 41 c-20 23 -73 80 -118 127 -45 47 -121 128 -171
+180 -49 52 -128 136 -175 185 -47 50 -157 167 -245 260 -88 94 -198 211 -245
+260 -385 405 -396 418 -463 550 -92 183 -111 265 -112 480 0 147 3 182 23 255
+83 303 263 530 537 675 235 125 544 149 805 64 168 -55 317 -155 470 -315 169
+-178 291 -307 580 -614 91 -96 203 -215 250 -265 47 -49 122 -128 166 -175
+177 -188 223 -234 227 -230 12 12 60 256 74 370 20 165 13 430 -16 595 -101
+573 -400 1053 -873 1403 -273 201 -668 353 -1020 391 -89 10 -383 13 -453 4z"/>
+                        <path d="M2765 2159 c-27 -6 -88 -28 -135 -51 -151 -72 -274 -221 -321 -388
+-7 -25 -13 -94 -13 -155 1 -87 5 -123 23 -175 44 -126 76 -170 305 -410 45
+-47 119 -126 166 -175 46 -50 125 -133 175 -185 49 -52 128 -136 174 -185 280
+-298 333 -342 461 -385 91 -31 243 -38 330 -15 152 40 272 123 355 245 136
+201 140 466 10 663 -40 59 -51 71 -375 412 -47 50 -157 167 -245 260 -378 402
+-400 425 -475 470 -88 53 -171 77 -285 81 -55 2 -122 -1 -150 -7z"/>
+                    </g>
+                </svg>
+            </div>
+            <div class="nav__list">
+                <a class="nav__list-item" href="/">Головна</a>
+                <a class="nav__list-item" modal-open="rules">Опис привілеї</a>
+                <a class="nav__list-item" href=#">Наш Discord</a>
+                <a class="nav__list-item" href=#">Підтримка</a>
+            </div>
+            <div class="nav__control">
+                <div class="nav__control-item nav__control-online">
+                    <img src="../assets/images/icons/activity.svg" class="nav__control-online__icon" alt="">
+                    <div class="nav__control-online__info ">
+                        <p class="nav__control-online__hover">Натисніть, щоб скоюпіювати id серверу</p>
+                        <p class="nav__control-online__ip ">quickworld.fun</p>
+                        <p class="nav__control-online__status">Онлайн: <span id="server0"
+                                                                             class="nav__control-online__checked">999</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="nav__control-item nav__control-theme theme">
+                    <img src="../assets/images/icons/moon.svg" class="nav__control-theme__icon moon" alt="">
+                    <img src="../assets/images/icons/sun.svg" class="nav__control-theme__icon sun disabled" alt="">
+                </div>
+                <div class="nav__control-item nav__control-burger">
+                    <div class="nav__control-burger__line nav__control-burger__line1"></div>
+                    <div class="nav__control-burger__line nav__control-burger__line2"></div>
+                    <div class="nav__control-burger__line nav__control-burger__line3"></div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <section class="main_banner" style="padding-bottom: 6%;padding-top: 6%;">>
+        <div class="_container">
+            <div class="slider_item">
+                <h2>Покупка була успішною!</h2>
+                <p>Ресурси вже були вам доставленi. При помилкових випадках - писати в тп.</p>
+                <a href="/">Головна</a>
+            </div>
+        </div>
+    </section>
+
+
+
+    <footer class="footer">
+        <div class="_container">
+            <div class="footer_left">
+                <div class="footer_logo">
+                    <a href="">
+                        <svg version="2.0" xmlns="http://www.w3.org/2000/svg"
+                             width="40.000000pt" height="40.000000pt" viewBox="0 0 440.000000 443.000000"
+                             preserveAspectRatio="xMidYMid meet">
+
+                            <g transform="translate(0.000000,443.000000) scale(0.100000,-0.100000)"
+                               fill="white" stroke="none">
+                                <path d="M1965 4403 c-284 -33 -533 -106 -767 -224 -426 -214 -783 -583 -985
+-1019 -274 -592 -268 -1269 17 -1855 361 -743 1100 -1212 1920 -1218 211 -2
+324 10 518 54 l93 21 -37 41 c-20 23 -73 80 -118 127 -45 47 -121 128 -171
+180 -49 52 -128 136 -175 185 -47 50 -157 167 -245 260 -88 94 -198 211 -245
+260 -385 405 -396 418 -463 550 -92 183 -111 265 -112 480 0 147 3 182 23 255
+83 303 263 530 537 675 235 125 544 149 805 64 168 -55 317 -155 470 -315 169
+-178 291 -307 580 -614 91 -96 203 -215 250 -265 47 -49 122 -128 166 -175
+177 -188 223 -234 227 -230 12 12 60 256 74 370 20 165 13 430 -16 595 -101
+573 -400 1053 -873 1403 -273 201 -668 353 -1020 391 -89 10 -383 13 -453 4z"/>
+                                <path d="M2765 2159 c-27 -6 -88 -28 -135 -51 -151 -72 -274 -221 -321 -388
+-7 -25 -13 -94 -13 -155 1 -87 5 -123 23 -175 44 -126 76 -170 305 -410 45
+-47 119 -126 166 -175 46 -50 125 -133 175 -185 49 -52 128 -136 174 -185 280
+-298 333 -342 461 -385 91 -31 243 -38 330 -15 152 40 272 123 355 245 136
+201 140 466 10 663 -40 59 -51 71 -375 412 -47 50 -157 167 -245 260 -378 402
+-400 425 -475 470 -88 53 -171 77 -285 81 -55 2 -122 -1 -150 -7z"/>
+                            </g>
+                        </svg>
+                    </a>
+                    <div class="footer_name">
+                        <p>Quick World 2022 ©</p>
+                        <span>ФОП Токар В.М. | 3403401595</span>
+                    </div>
+                </div>
+
+                <div class="footer_payment">
+                    <img src="assets/images/visa.svg" alt="">
+                    <img src="assets/images/MasterCard_1979_logo.svg" alt="">
+                </div>
+            </div>
+
+            <div class="footer_right">
+                <div class="navigation">
+                    <h3>Навігація:</h3>
+                    <div class="footer_nav_bar">
+                        <div class="footer_nav_left">
+                            <a href="#">Контакти</a>
+                            <a href="#">Угода</a>
+
+                        </div>
+                        <div class="footer_nav_right">
+                            <a href="#">Політика безпеки та повернення</a>
+                            <a href="#"> Політика оплати</a>
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/18846f0aad.js" crossorigin="anonymous"></script>
+<script src="../assets/scripts/popup.js"></script>
+<script src="../assets/scripts/script.js"></script>
+<script src="../assets/scripts/main.js"></script>
+</body>
+</html>
