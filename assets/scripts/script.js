a87145273b51ae07de52e90ac609c5d117fd5989
tokarvasyl1 <tokarvasyl1@gmail.com>
2022-08-20 14:15:16 +0300

    popup

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.theme').click(function (event) {
-        $('.nav, .main_banner, .donate, .footer, .nav__list-item, .nav__logo, .donate__products-title, .donate__sidebar-header, .donate__products-name, .donate__products-price, .donate__products-button, .donate__sidebar-item, .donate__products-item').toggleClass('dark');
+        $('.nav, .main_banner, .donate, .footer, .nav__list-item, .nav__logo, .donate__products-title,.donate__products-buy, .donate__sidebar-header, .donate__products-name, .donate__products-price, .donate__products-button, .donate__sidebar-item, .donate__products-item, .theme').toggleClass('dark');
     })
 })
 
